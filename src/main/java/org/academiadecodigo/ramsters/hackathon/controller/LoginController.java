76bdd0eb28c73b84806564a86225693fd7bdc937
joaolopes241 <jallopes241@gmail.com>
2020-04-28 00:17:00 +0100

    controllers update

--- src/main/java/org/academiadecodigo/ramsters/hackathon/controller/LoginController.java
+++ src/main/java/org/academiadecodigo/ramsters/hackathon/controller/LoginController.java
@@ -1,10 +1,20 @@
 package org.academiadecodigo.ramsters.hackathon.controller;
 
 import org.academiadecodigo.ramsters.hackathon.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/login")
 public class LoginController {
 
     private AuthService authService;
 
+    @Autowired
+    public void setAuthService(AuthService authService) {
+        this.authService = authService;
+    }
 
+    public String login() {}
 }
